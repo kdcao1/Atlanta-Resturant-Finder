62f72b585466639fb5aff6016cce1e16438e12a4
kdcao1 <kdcao1@yahoo.com>
2024-09-06 00:54:08 -0400

    Initial Commit

---
+++ Atlanta_Food_Finder/Atlanta_Food_Finder/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Atlanta_Food_Finder project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Atlanta_Food_Finder.settings')
+
+application = get_wsgi_application()
