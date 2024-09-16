66e7382f857708b5dcf0ea9b18c13eed866253f0
kdcao1 <kdcao1@yahoo.com>
2024-09-16 19:33:32 -0400

    Login Fix
    - Fixed login check
    - Shows error with incorrect login

--- Atlanta_Food_Finder/foodFinder/views.py
+++ Atlanta_Food_Finder/foodFinder/views.py
@@ -1,3 +1,5 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
@@ -9,21 +11,27 @@ def home(request):
     return render(request, "foodFinder/home.html", context=None)
 
 def logins(request):
+    login_error = False
     if request.method == "POST":
-        username = request.POST.get('usernameInput')
-        password = request.POST.get('passwordInput')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
         # Authenticate the user with the provided username and password
         user = authenticate(username=username, password=password)
 
         if user is None:
             # Display an error message if authentication fails (invalid password)
-            messages.error(request, "Invalid Password")
+            print("Bad Login")
+            login_error = True
         else:
             # Log in the user and redirect to the home page upon successful login
+            print("Good Login")
             login(request, user)
             return redirect('/foodFinder')
-    return render(request, "foodFinder/login.html", context=None)
+    context = {
+        "login_error": login_error
+    }
+    return render(request, "foodFinder/login.html", context)
 
 def favorites(request):
     return render(request, "foodFinder/favorites.html", context=None)
