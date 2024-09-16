420fc1d1f42ea3cfc02ea641b9a8c6f9433c463d
kdcao1 <kdcao1@yahoo.com>
2024-09-16 13:33:15 -0400

    Fixed login method
    Fixed login method

--- Atlanta_Food_Finder/foodFinder/views.py
+++ Atlanta_Food_Finder/foodFinder/views.py
@@ -10,14 +10,8 @@ def home(request):
 
 def logins(request):
     if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        # Check if a user with the provided username exists
-        if not User.objects.filter(username=username).exists():
-            # Display an error message if the username does not exist
-            messages.error(request, 'Invalid Username')
-            return redirect('/login/')
+        username = request.POST.get('usernameInput')
+        password = request.POST.get('passwordInput')
 
         # Authenticate the user with the provided username and password
         user = authenticate(username=username, password=password)
@@ -25,11 +19,10 @@ def logins(request):
         if user is None:
             # Display an error message if authentication fails (invalid password)
             messages.error(request, "Invalid Password")
-            return redirect('/login/')
         else:
             # Log in the user and redirect to the home page upon successful login
             login(request, user)
-            return redirect('/home/')
+            return redirect('/foodFinder')
     return render(request, "foodFinder/login.html", context=None)
 
 def favorites(request):
